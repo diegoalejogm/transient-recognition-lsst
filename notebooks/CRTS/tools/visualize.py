4b7c14b5deb2af3ab1dcdad4335469894b3f4dbd
Diego Gomez Mosquera <diegoalejogm@gmail.com>
2017-10-03 16:29:09 -0500

    Added more image visualizations techniques.

--- notebooks/CRTS/tools/visualize.py
+++ notebooks/CRTS/tools/visualize.py
@@ -1,5 +1,5 @@
 from . import time, io
-from . import constants as k 
+from . import constants as k
 from plotly.offline import init_notebook_mode
 import plotly.graph_objs as go
 import pandas as pd
@@ -23,21 +23,23 @@ def light_curve_interactive_scatter(obj_light_curve_df, mode='markers', name=Non
 
 
 def light_curve(df, filename, mode='markers'):
-    io.makedir(k.DIR_IMAGES_SCATTERS)
+    outdir = k.DIR_IMAGES_SCATTERS; io.makedir(outdir)
+    if io.file_exists(filename, outdir): return
     plot_df = df.plot(x='Date', y='Mag', style='.', ms=.6, figsize=(20,15))
     plot_df.autoscale(tight=False)
-    plt.savefig(k.DIR_IMAGES_SCATTERS + filename + '.png', dpi=150)
+    plt.savefig(outdir + filename + '.png', dpi=150)
     plt.close(plot_df.get_figure())
 
 
 def combined_light_curves(df_lc, n_obj, obj_type, min_obs=None, filtre=None, random_state=182):
     df_lc = df_lc.copy()
     np.random.seed(random_state)
-    # Sample data
-    if min_obs: 
+    # Filter data
+    if min_obs:
         s_grouped = df_lc.groupby(['ObjectID']).count()['ID']
         valid_obs = s_grouped[s_grouped >= min_obs].index.tolist()
         df_lc = df_lc[df_lc.ObjectID.isin(valid_obs)]
+    # Sample data
     sample_obj_ids = np.random.choice(df_lc.ObjectID.unique(), n_obj, replace=False)
     df_lc_sample = df_lc[df_lc.ObjectID.isin(sample_obj_ids)].copy()
     # Separate LCs into seasons
@@ -45,65 +47,137 @@ def combined_light_curves(df_lc, n_obj, obj_type, min_obs=None, filtre=None, ran
         prev_MJD, season_index = None, -1
         for lc_row_index, lc_row in df_lc_sample[df_lc_sample.ObjectID == obj_id].sort_values(['MJD']).iterrows():
             current_MJD = lc_row.MJD
-            if prev_MJD == None or (current_MJD - prev_MJD) >= 180:
-                prev_MJD, season_index = current_MJD, season_index+1
+            if prev_MJD == None or (current_MJD - prev_MJD) >= 150:
+                season_index = season_index+1
             df_lc_sample.loc[lc_row_index, 'Season'] = season_index
+            prev_MJD = current_MJD
     df_lc_sample['Season'] = df_lc_sample['Season'].astype(int)
-    
-    figsize = (20,8)
-    # Draw LCs for each object, using all seasons
-    __combined_light_curves_joint__(obj_type, sample_obj_ids, df_lc_sample, figsize)
-    # Draw LCs for each object, per season
-    __combined_light_curves_per_season__(obj_type, sample_obj_ids, df_lc_sample, figsize)
 
-        
-def __combined_light_curves_joint__(obj_type, sample_obj_ids, df_lc_sample, figsize):
-    curves_season_out_dir = k.DIR_IMAGES_CURVES + str(obj_type) + '/all-seasons/'
-    io.makedir(curves_season_out_dir)
+    figsize = (20,8)
+    dict_plotting_data = __generate_plotting_data__(sample_obj_ids, df_lc_sample)
+    # Draw LCs for each object, using all seasons
+    __combined_light_curves_all_seasons__(obj_type, dict_plotting_data, figsize, local=False)
+    __combined_light_curves_all_seasons__(obj_type, dict_plotting_data, figsize, local=True)
+    # Draw LCs for each object type, using all seasons
+    __combined_light_curves_all_seasons_by_type__(obj_type, dict_plotting_data, figsize, 3)
+    # Draw LCs for each object, per season
+    __combined_light_curves_per_season__(obj_type, dict_plotting_data, figsize)
+
+
+def __generate_plotting_data__(sample_obj_ids, df_lc_sample):
+    plotting_data_dict = dict()
+    seasons_list = sorted(df_lc_sample.Season.unique())
     for obj_id in sample_obj_ids:
-        for season_num in sorted(df_lc_sample.Season.unique()):
-            filename = curves_season_out_dir
+        s_resampled_list, s_resampled_err_list, s_interpolated_list  = [], [], []
+        obj_seasons_list = []
+        for season_num in seasons_list:
             df_current = df_lc_sample[(df_lc_sample.ObjectID == obj_id) & (df_lc_sample.Season == season_num)]
+            # Pass if current season for ObjectID is empty
             if df_current.empty: continue
             # Resample data using mean
             s_resampled = df_current.resample('D',on='Date').mean()['Mag']
-            plot_resampled = s_resampled.plot(figsize=figsize, style='.')
-            plot_resampled.autoscale(tight=False)
+            s_resampled_err = df_current.resample('D',on='Date').mean()['Magerr']
             # Interpolate data using splines
             n_rows = s_resampled[s_resampled.notnull()].shape[0]
-            interpolation_order = 3 if n_rows >= 5 else (2 if n_rows == 4 else 1)
-            s_interpolated = s_resampled.interpolate(method='spline', order=interpolation_order)
+            order = 3 if n_rows >= 5 else (2 if n_rows >= 3 else 1)
+            w = (1/s_resampled[s_resampled.notnull()]).values
+            s_interpolated = s_resampled.interpolate(method='spline', order=order, w=w)
+#            s_interpolated = s_resampled.interpolate(method='spline', order=order)
+            s_interpolated.loc[s_resampled[s_resampled.notnull()].index] = np.NaN
+            s_interpolated = s_interpolated.interpolate(method='spline', order=order)
+            # Add data to dict
+            s_resampled_list.append(s_resampled)
+            s_resampled_err_list.append(s_resampled_err)
+            s_interpolated_list.append(s_interpolated)
+            obj_seasons_list.append(season_num)
+        obj_dict = dict()
+        obj_dict['s_resampled_mag'] = s_resampled_list
+        obj_dict['s_resampled_magerr'] = s_resampled_err_list
+        obj_dict['s_interpolated_mag'] = s_interpolated_list
+        obj_dict['seasons_list'] = obj_seasons_list
+        plotting_data_dict[str(obj_id)] = obj_dict
+    return plotting_data_dict
+
+
+def __combined_light_curves_all_seasons__(obj_type, plotting_data, figsize, local):
+    folder = 'local' if local else 'global'
+    out_dir = k.DIR_IMAGES_CURVES + '{}/all-seasons/{}/'.format(str(obj_type), folder)
+    io.makedir(out_dir)
+    for obj_id, obj_data in plotting_data.items():
+        s_resampled_list = obj_data['s_resampled_mag']
+        s_resampled_err_list = obj_data['s_resampled_magerr']
+        s_interpolated_list = obj_data['s_interpolated_mag']
+        seasons_list = obj_data['seasons_list']
+        filename = '{}.png'.format(obj_id)
+        if io.file_exists(filename, out_dir, False): continue
+        for i, season_num in enumerate(seasons_list):
+            s_resampled = s_resampled_list[i]
+            s_resampled_err = s_resampled_err_list[i]
+            s_interpolated = s_interpolated_list[i]
+            yerr = s_resampled_err
+            # Plot known datapoints with error bars
+            elw = .3 if local else .5
+            plot_resampled = s_resampled.plot(figsize=figsize, yerr=yerr, fmt='.', elinewidth=elw)
+            plot_resampled.autoscale(tight=False)
             # Plot interpolated data
-            plot_interpolated = s_interpolated.plot(figsize=figsize, color=plot_resampled.get_lines()[-1].get_color())
+            last_color=plot_resampled.get_lines()[-1].get_color()
+            plot_interpolated = s_interpolated.plot(figsize=figsize, color=last_color)
             plot_interpolated.autoscale(tight=False)
-            # Save plot
-        plot_interpolated.get_figure().savefig(filename + '{}.png'.format(obj_id), dpi=110)
-        plot_interpolated.set_ylim(8, 24)
-        plot_interpolated.get_figure().savefig(filename + 'full_{}.png'.format(obj_id), dpi=110)
+        # Save plot
+        if not local: plot_interpolated.set_ylim(8, 24)
+        plot_interpolated.get_figure().savefig(out_dir + filename, dpi=110)
         # Close plot
         plt.close(plot_interpolated.get_figure())
-        
-def __combined_light_curves_per_season__(obj_type, sample_obj_ids, df_lc_sample, figsize):
-    for season_num in sorted(df_lc_sample.Season.unique()):
-        curves_season_out_dir = k.DIR_IMAGES_CURVES + str(obj_type) + '/season{}/'.format(season_num)
-        io.makedir(curves_season_out_dir)
-        for obj_id in sample_obj_ids:
-            filename = curves_season_out_dir + '{}.png'.format(obj_id)
-            df_current = df_lc_sample[(df_lc_sample.ObjectID == obj_id) & (df_lc_sample.Season == season_num)]
-            if df_current.empty: continue
-            # Resample data using mean
-            s_resampled = df_current.resample('D',on='Date').mean()['Mag']
-            plot_resampled = s_resampled.plot(figsize=figsize, style='.')
-            plot_resampled.autoscale(tight=False)
-            n_rows = s_resampled[s_resampled.notnull()].shape[0]
-            # Interpolate data using splines
-            interpolation_order = 3 if n_rows >= 5 else (2 if n_rows == 4 else 1)
-            s_interpolated = s_resampled.interpolate(method='spline', order=interpolation_order)
+
+def __combined_light_curves_all_seasons_by_type__(obj_type, plotting_data, figsize, N):
+    out_dir = k.DIR_IMAGES_CURVES + '{}/all-seasons/'.format(str(obj_type))
+    io.makedir(out_dir)
+    filename = '{}.png'.format(obj_type)
+    if io.file_exists(filename, out_dir, False): return
+    for index, key in enumerate(plotting_data):
+        if index == N: break
+        obj_data = plotting_data[key]
+        s_resampled_list = obj_data['s_resampled_mag']
+        s_resampled_err_list = obj_data['s_resampled_magerr']
+        s_interpolated_list = obj_data['s_interpolated_mag']
+        seasons_list = obj_data['seasons_list']
+        for i, season_num in enumerate(seasons_list):
+            s_interpolated = s_interpolated_list[i]
             # Plot interpolated data
-            plot_interpolated = s_interpolated.plot(figsize=figsize, color=plot_resampled.get_lines()[-1].get_color())
+            last_color = None if i==0 else plot_interpolated.get_lines()[-1].get_color()
+            plot_interpolated = s_interpolated.plot(figsize=figsize, color=last_color)
+    # Save plot
+    plot_interpolated.set_ylim(8, 24)
+    plot_interpolated.get_figure().savefig(out_dir + filename, dpi=110)
+    # Close plot
+    plt.close(plot_interpolated.get_figure())
+
+
+def __combined_light_curves_per_season__(obj_type, plotting_data, figsize):
+    for obj_id, obj_data in plotting_data.items():
+        # Make out dir
+        out_dir = k.DIR_IMAGES_CURVES + '{}/per-season/{}/'.format(str(obj_type), obj_id)
+        io.makedir(out_dir)
+        s_resampled_list = obj_data['s_resampled_mag']
+        s_resampled_err_list = obj_data['s_resampled_magerr']
+        s_interpolated_list = obj_data['s_interpolated_mag']
+        seasons_list = obj_data['seasons_list']
+        for i, season_num in enumerate(seasons_list):
+            filename = '{}.png'.format(i)
+            if io.file_exists(filename, out_dir, False): continue
+            # Get data
+            s_resampled = s_resampled_list[i]
+            s_resampled_err = s_resampled_err_list[i]
+            s_interpolated = s_interpolated_list[i]
+            # Plot known datapoints with error bars
+            yerr = s_resampled_err
+            plot_resampled = s_resampled.plot(figsize=figsize, yerr=yerr, fmt='.', elinewidth=.3)
+            plot_resampled.autoscale(tight=False)
+            # Plot interpolated data
+            last_color=plot_resampled.get_lines()[-1].get_color()
+            plot_interpolated = s_interpolated.plot(figsize=figsize, color=last_color)
             plot_interpolated.autoscale(tight=False)
             # Save plot
-            plot_interpolated.get_figure().savefig(filename, dpi=80)
+            plot_interpolated.get_figure().savefig(out_dir + filename, dpi=80)
             # Close plot
             plt.close(plot_interpolated.get_figure())
-    
